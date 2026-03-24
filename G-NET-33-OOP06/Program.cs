ebe17091dde0f730e56c29f90cd707fac1d701bf
MO <mohamedxehab0@gmail.com>
2026-03-24 19:11:10 +0200

    Add project files.

---
+++ G-NET-33-OOP06/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace G_NET_33_OOP06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part1
+            #region A1
+            /*
+             1-Abstraction means hiding complexity 
+               and show only the features of the object.
+
+            2-Encapsulation means protect data by controlling 
+              who can access and modify it.
+
+            3-When you play video game like Battle royal you shoot, move, reload
+              but you don't know how bullet are calculated or 
+              how the player move this is Abstraction 
+            4-The player have heal, and number of bullets you can't change it 
+              directly you only need to do an action to change it 
+             */
+            #endregion
+            #region A2
+            /*
+             1-                 Abstraction                         Interface
+            constructor          Yes                                No
+            Inheritance         only one abstract class             Yes 
+            Methods             Can have abstract +                 Usually only method declarations
+                                concrete (implemented) methods
+            Access Modifiers    Can use private, protected, public   Only public
+
+            When to use Abstraction over Interface
+            1-Classes share common state (fields)
+            2-You want to provide default behavior
+            3-There is a clear base class relationship
+             */
+            #endregion
+            #region A3
+            #endregion
+            #region A4
+            #endregion
+            #endregion
+        }
+    }
+}
